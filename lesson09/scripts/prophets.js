86410130640fcfe9f9164ede82cc7656e117f9a0
JPgitidaho <122384707+JPgitidaho@users.noreply.github.com>
2024-03-07 12:16:44 -0300

    actualization

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -1,41 +1,41 @@
-const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
 const cardsContainer = document.querySelector('#cards');
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+
+const displayProphets = (prophets) => {
+  prophets.forEach((prophet) => {
+    let card = document.createElement('section');
+    let fullName = document.createElement('h2');
+    let portrait = document.createElement('img');
+
+    fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+    const dateInfo = document.createElement('p');
+    dateInfo.textContent = `${prophet.birthdate} - ${prophet.death}`;
+
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+    portrait.setAttribute('loading', 'lazy');
+    portrait.setAttribute('width', '340');
+    portrait.setAttribute('height', '440');
+
+    card.appendChild(fullName);
+    card.appendChild(dateInfo);
+    card.appendChild(portrait);
+
+    // Agrega la clase 'card' para aplicar estilos CSS
+    card.classList.add('card');
+
+    cardsContainer.appendChild(card);
+  });
+}
 
 async function getProphetData() {
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-
-        // Itera sobre los profetas y crea las tarjetas
-        data.prophets.forEach(prophet => {
-            const card = document.createElement('div');
-            card.classList.add('card');
-
-            // Crea la imagen
-            const image = document.createElement('img');
-            image.src = prophet.imageurl;
-            image.alt = `${prophet.name} ${prophet.lastname}`;
-
-            // Crea un elemento de encabezado h2 para el nombre
-            const nameHeader = document.createElement('h2');
-            nameHeader.textContent = `${prophet.name} ${prophet.lastname}`;
-
-            // Crea un párrafo para la información de fecha de nacimiento y muerte
-            const dateInfo = document.createElement('p');
-            dateInfo.textContent = `${prophet.birthdate} - ${prophet.death}`;
-
-            // Añade la imagen, el nombre y la información de fecha a la tarjeta
-            card.appendChild(image);
-            card.appendChild(nameHeader);
-            card.appendChild(dateInfo);
-
-            // Añade la tarjeta al contenedor
-            cardsContainer.appendChild(card);
-        });
-
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayProphets(data.prophets);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
 }
 
 getProphetData();
