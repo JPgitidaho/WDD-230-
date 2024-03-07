2705b1637e74e863dc3ddf38b4dd7871752d88e0
JPgitidaho <122384707+JPgitidaho@users.noreply.github.com>
2024-03-07 11:51:07 -0300

    actualization

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -16,8 +16,18 @@ async function getProphetData() {
             image.src = prophet.imageurl;
             image.alt = `${prophet.name} ${prophet.lastname}`;
 
-            // Añade la imagen a la tarjeta
+            // Crea un elemento de encabezado h2 para el nombre
+            const nameHeader = document.createElement('h2');
+            nameHeader.textContent = `${prophet.name} ${prophet.lastname}`;
+
+            // Crea un párrafo para la información de fecha de nacimiento y muerte
+            const dateInfo = document.createElement('p');
+            dateInfo.textContent = `${prophet.birthdate} - ${prophet.death}`;
+
+            // Añade la imagen, el nombre y la información de fecha a la tarjeta
             card.appendChild(image);
+            card.appendChild(nameHeader);
+            card.appendChild(dateInfo);
 
             // Añade la tarjeta al contenedor
             cardsContainer.appendChild(card);
