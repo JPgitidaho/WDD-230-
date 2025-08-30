4392fc62fa546529361f21033cd192bb066a29a2
JPgitidaho <juanichile2015@gmail.com>
2025-08-30 19:04:30 -0400

    actualization

--- scripts/visitcount.js
+++ scripts/visitcount.js
@@ -1,17 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Retrieve the visit counter from localStorage
+   
     let visitCount = localStorage.getItem("visitCount");
   
-    // Check if it's the first visit or not
+   
     if (visitCount === null) {
       visitCount = 1;
     } else {
       visitCount = parseInt(visitCount) + 1;
     }
-  
-    // Display the counter in the HTML element
+
     document.getElementById("visit-counter").textContent = visitCount;
   
-    // Save the new counter value to localStorage
     localStorage.setItem("visitCount", visitCount.toString());
 });
--- scripts/weather.js
+++ scripts/weather.js
@@ -6,26 +6,25 @@ document.addEventListener('DOMContentLoaded', function () {
 // Function to fetch weather data from the OpenWeatherMap API
 async function getWeather(city, country) {
   try {
-    // OpenWeatherMap API key
+
     const apiKey = '39a93938e81e6967af92542761303653';
-    // API URL with specific city and country
+ 
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}`;
 
-    // Fetch weather data from the API
+   
     const response = await fetch(apiUrl);
     const data = await response.json();
 
-    // Extract relevant information from the data object
+  
     const weatherDescription = data.weather[0].description;
     const temperature = (data.main.temp - 273.15).toFixed(2); // Temperature in Celsius
     const feelsLike = (data.main.feels_like - 273.15).toFixed(2); // Feels like temperature in Celsius
     const humidity = data.main.humidity;
     const windSpeed = data.wind.speed;
 
-    // Get the weather icon 
+
     const weatherIcon = getWeatherIcon(weatherDescription);
 
-    // Create the content to display on the page
     const weatherInfo = `
       <p>Weather Description: ${weatherDescription} ${weatherIcon}</p>
       <p>Temperature: ${temperature} °C</p>
@@ -34,16 +33,15 @@ async function getWeather(city, country) {
       <p>Wind Speed: ${windSpeed} m/s</p>
     `;
 
-    // Display the information in the element with the id 'weather-info'
+  
     document.getElementById('weather-info').innerHTML = weatherInfo;
   } catch (error) {
     console.error('Error fetching weather information:', error);
   }
 }
 
-// Function to get the weather icon based on the weather description
 function getWeatherIcon(description) {
-  // Map descriptions to icon names
+
   const iconMap = {
     'clear sky': '☀️',
     'few clouds': '🌤️',
@@ -56,6 +54,6 @@ function getWeatherIcon(description) {
     'mist': '🌫️'
   };
 
-  // Look up the corresponding icon or use a generic icon if there is no match
+
   return iconMap[description.toLowerCase()] || '❓';
 }
